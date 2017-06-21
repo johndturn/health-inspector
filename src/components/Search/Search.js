6c2d797079eb50c1cb7b99a7262fc5bca6e4cdcf
John Turner <john.duane.turner@gmail.com>
2017-06-21 17:54:56 +0000

    Merged in add-css-animations (pull request #8)
    Added CSS animations to the searching container
    
    Approved-by: Derik Hasvold <derik.hasvold@gmail.com>

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,9 +2,27 @@ import React from 'react'
 import './Search.css'
 
 class Search extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      shrink: false
+    }
+  }
+
+  componentDidMount() {
+    // TODO: Remove - just for testing
+    setTimeout(() => {
+      this.setState({shrink: true})
+    }, 3000)
+  }
+
   render() {
+    const classes = !this.state.shrink
+      ? 'search-container'
+      : 'search-container search-container-small'
     return (
-      <div className="search-container" />
+      <div className={classes} />
     )
   }
 }
